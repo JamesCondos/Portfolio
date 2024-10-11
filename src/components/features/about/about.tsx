a0af785aee87356ea743c855e4a5368df3021345
JamesCondos <Genie@Condos.com>
2024-10-11 13:16:13 +1100

    movedresume

--- src/components/features/about/about.tsx
+++ src/components/features/about/about.tsx
@@ -20,7 +20,7 @@ const About = React.forwardRef<HTMLDivElement>((props, ref) => {
         <br />
 
         <ul className = "actions">
-										<li><a href="src\components\features\works\JamesCondos_Resume_Aug_2024.pdf" download className ="button">Download my Resume!</a></li>
+										<li><a href="\JamesCondos_Resume_Aug_2024.pdf" download className ="button">Download my Resume!</a></li>
 				</ul>
        
       </p>
